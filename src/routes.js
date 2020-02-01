27a70334a520fad77427d8fe36ccec214d476e26
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-01 14:05:28 -0300

    Creates app models and database index

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Heallo AA' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme Augusto',
+    email: 'b@b.com',
+    password_hash: '123456',
+    provider: false,
+  });
+  return res.json(user);
 });
 
 export default routes;
