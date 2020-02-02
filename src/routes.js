fe3e4b22fe64d97d78a0689d21556136bd7af564
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-02 17:38:10 -0300

    Creates route to read notifications

--- src/routes.js
+++ src/routes.js
@@ -26,6 +26,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
