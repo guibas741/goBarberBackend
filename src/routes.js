07288875005fdb0c227d2e9b823bb633378604ae
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-02 15:22:11 -0300

    List schedule of appointments of provider user

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
