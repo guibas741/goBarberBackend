022db7aa2eead5c9ff7d7956e1c7a321d0ff42a0
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-02 13:43:33 -0300

    List providers with avatar and url to see the avatar image

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -14,6 +15,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
