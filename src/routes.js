8f1f14279d6c6d5e7748c5dbe1b79366de8d0052
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-02 14:41:35 -0300

    Creates AppointmentController and route to store appointments

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +17,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
