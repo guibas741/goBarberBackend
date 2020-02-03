8a22e41904e952194766517aa0b785c587c519b4
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-02 22:51:39 -0300

    Configures nodemailer and creates sendMail method

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e75f03da6bd2f8',
+    pass: '8afcfc3ece333b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
