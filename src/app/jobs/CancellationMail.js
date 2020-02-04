d435c6879c20d6087e11b15c3ac73388a4d242d5
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-04 08:27:03 -0300

    Handles error on queue

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -9,7 +9,7 @@ class CancellationMail {
 
   async handle({ data }) {
     const { appointment } = data;
-    console.log('EXECUTO');
+
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento Cancelado',
